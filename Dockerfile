c8a1532e665d3ad91826c781e8a7e5a39b8067c3
yugyeongh <uugyeong27@gmail.com>
2023-03-23 21:53:31 +0900

    edit dockerfile notion page id

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ COPY package.json ./
 COPY yarn.lock ./
 
 #env setting -> 각자의 NOTION_PAGE_ID로
-ENV NOTION_PAGE_ID 1e8328955e0c4b0bb297b43183df42d1
+ENV NOTION_PAGE_ID 1e8328955e0c4b0bb297b43183df42d1?v=02320a48bb9a40edbb25bc0c14b2b3bc
 
 #의존성 설치
 RUN yarn
--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ['www.notion.so'],
+    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
 }
-
-// , 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'
